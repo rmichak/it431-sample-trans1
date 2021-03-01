fd9cde02a6e99053150245ea8d3e93149bf90378
randy <randy.michak@761link.net>
2021-02-28 20:09:45 -0500

    Add project files.

---
+++ Trans1/Data/CourseDatabaseInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Trans1.Models;
+
+namespace Trans1.Data
+{
+    public class Trans1DBInitializer : DropCreateDatabaseAlways<Trans1Context>
+    {
+        protected override void Seed(Trans1Context context)
+        {
+
+            IList<Course> defaultCourses = new List<Course>();
+
+            defaultCourses.Add(new Course() { Name = "Course 1", Credits=3, Term="Spring", Grade="A" });
+            defaultCourses.Add(new Course() { Name = "Course 2", Credits = 3, Term = "Spring", Grade = "A" });
+            defaultCourses.Add(new Course() { Name = "Course 3", Credits = 3, Term = "Spring", Grade = "A" });
+            defaultCourses.Add(new Course() { Name = "Course 4", Credits = 3, Term = "Spring", Grade = "A" });
+            defaultCourses.Add(new Course() { Name = "Course 5", Credits = 3, Term = "Spring", Grade = "A" });
+
+            context.Courses.AddRange(defaultCourses);
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ Trans1/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Trans1.Models
+{
+    public class Course
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Credits { get; set; }
+        [Required]
+        public string Term { get; set; }
+        [Required]
+        public string Grade { get; set; }
+
+    }
+}
